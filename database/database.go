8a07664ef35a88b96bd5630e99f52647e9fcfa34
Renaud <renaudgdumont@gmail.com>
2021-08-02 20:31:21 -0400

    Cleaner way to get the database instance

--- database/database.go
+++ database/database.go
@@ -9,18 +9,9 @@ import (
 	"gorm.io/gorm"
 )
 
-var db *DB
-
-type DB struct {
-	*gorm.DB // or what database you want like *mongo.Client
-}
-
-func GetDB() *DB {
-	if db == nil {
-		db.DB = InitDatabase()
-	}
-	return db
-}
+var (
+	dbInstance *gorm.DB
+)
 
 func InitDatabase() *gorm.DB {
 	fmt.Println("Initializing database...")
@@ -31,19 +22,19 @@ func InitDatabase() *gorm.DB {
 		dns = "host=0.0.0.0 user=quickventory password=123456 dbname=quickventory port=5432 sslmode=disable TimeZone=America/Toronto"
 	}
 	var err error
-	db.DB, err = gorm.Open(postgres.Open(dns), &gorm.Config{})
+	dbInstance, err = gorm.Open(postgres.Open(dns), &gorm.Config{})
 	if err != nil {
 		panic("Connection to database failed")
 	}
 
-	err = migrate(db.DB)
+	err = migrate(dbInstance)
 	if err != nil {
 		fmt.Println("Error migrating database: ", err)
 	} else {
 		fmt.Println("Database migrated successfully")
 	}
 
-	return db.DB
+	return dbInstance
 }
 
 func migrate(dbInstance *gorm.DB) error {
