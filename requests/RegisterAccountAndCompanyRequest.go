0a421ac2b383706a2b328b6ad4087b1a0def19c6
Michał Isalski <micha_i@interia.pl>
2021-08-14 02:19:33 +0200

    Go lang (#1)
    * add restricted key to test stripe
    
    * Testing golang backend
    
    * base user model
    
    * create basic token alongside access to global database object
    
    * Cleaner way to get the database instance
    
    * Cleaner way to get the database instance
    
    * change user controllers
    
    * create validator file but do nothing cause im brain dead
    
    * create validator file but do nothing cause im brain dead
    
    * prepare to find dynamic struct based on route
    
    * clean up validation
    
    * test validation
    
    * fix routing through our requestValidationName
    
    * add comment as to why !condition and not condition
    
    * check if our request has a body and it is not empty
    
    * move routes to api and add middlewares
    
    * Add JWT support for tokens, works properly
    
    * Add a Store to the app and initialize it at initialization
    
    * check if token is expired
    
    * remove unneeded error
    
    * remove unneeded returns and throw exception on error
    
    * add a login contoller
    
    * login system done
    
    * wip on validation in controller
    
    * fix validator and fix token middleware
    
    * test push
    
    * fix token
    
    * attempt to integrate swagger
    
    * attempt to integrate swagger
    
    Co-authored-by: Renaud Dumont <renaudgdumont@gmail.com>

---
+++ requests/RegisterAccountAndCompanyRequest.go
@@ -0,0 +1,8 @@
+package requests
+
+type RegisterAccountAndCompanyRequest struct {
+	Email     string `json:"email"  binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+}
