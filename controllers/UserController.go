fe0be2d85772b87f7a41c66a0359166b856ee039
Renaud <renaudgdumont@gmail.com>
2021-08-16 13:16:17 -0400

    fix validation, taking break before redoing login

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -1,17 +1,27 @@
 package controllers
 
 import (
-	"errors"
 	"main/database"
 	"main/models"
-	"main/requests"
 	"main/store"
 	"main/utils"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
-	"gorm.io/gorm"
 )
+
+type RegisterAccountAndCompanyRequest struct {
+	Email     string `json:"email"  binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	FirstName string `json:"first_name" binding:"required"`
+	LastName  string `json:"last_name" binding:"required"`
+}
+
+type LoginRequest struct {
+	Email    string `json:"email"  binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
 
 // Login godoc
 // @Summary Login a user
@@ -21,34 +31,6 @@ import (
 // @Produce  json
 // @Router /users/login [post]
 func Login(c *gin.Context) {
-	// get email and password from request
-	var user models.User
-	email := c.PostForm("email")
-	password := c.PostForm("password")
-
-	record := database.Database.Model(&models.User{}).Where("email = ?", email).First(&user)
-	recordErr := record.Error
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-
-	if err != nil || errors.Is(recordErr, gorm.ErrRecordNotFound) {
-		// return invalid credentials error
-		c.JSON(400, gin.H{"message": "Invalid credentials"})
-		return
-	}
-
-	// check if password matches
-	if err := bcrypt.CompareHashAndPassword(hashedPassword, []byte(user.Password)); err != nil {
-		// return invalid credentials error
-		c.JSON(400, gin.H{"message": "Invalid credentials"})
-		return
-	}
-
-	tokenString := utils.GenerateTokenFromUser(&user, c)
-	// log our user in the state and return success
-	store.Store.User = user
-	// return token
-	c.JSON(200, gin.H{"token": tokenString})
-
 }
 
 // Register godoc
@@ -59,30 +41,32 @@ func Login(c *gin.Context) {
 // @Produce  json
 // @Router /users/register [post]
 func Register(c *gin.Context) {
+	var registrationData RegisterAccountAndCompanyRequest
 
-	if userValidated, ok := utils.Validate(requests.RegisterAccountAndCompanyRequest{}, c); ok {
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(userValidated["password"]), bcrypt.DefaultCost)
-		if err != nil {
-			c.JSON(400, gin.H{"message": "Exception occured"})
-			return
-		}
-		// create new user
-		user := models.User{
-			Email:     userValidated["email"],
-			Password:  string(hashedPassword),
-			FirstName: userValidated["first_name"],
-			LastName:  userValidated["last_name"],
-		}
-
-		database.Database.Create(&user)
-		// create access token for user and save it in database and return it
-		tokenString := utils.GenerateTokenFromUser(&user, c)
-
-		store.Store.User = user
-
-		c.JSON(200, gin.H{"token": tokenString})
+	if err := c.ShouldBindJSON(&registrationData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 	}
 
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(registrationData.Password), bcrypt.DefaultCost)
+	if err != nil {
+		c.JSON(400, gin.H{"message": "Exception occured"})
+		return
+	}
+	// create new user
+	user := models.User{
+		Email:     registrationData.Email,
+		Password:  string(hashedPassword),
+		FirstName: registrationData.FirstName,
+		LastName:  registrationData.LastName,
+	}
+
+	database.Database.Create(&user)
+	// create access token for user and save it in database and return it
+	tokenString := utils.GenerateTokenFromUser(&user, c)
+
+	c.Set("user", user)
+
+	c.JSON(200, gin.H{"token": tokenString})
 }
 
 func GetUser(c *gin.Context) {
--- requests/RegisterAccountAndCompanyRequest.go
+++
@@ -1,8 +0,0 @@
-package requests
-
-type RegisterAccountAndCompanyRequest struct {
-	Email     string `json:"email"  binding:"required"`
-	Password  string `json:"password" binding:"required"`
-	FirstName string `json:"first_name" binding:"required"`
-	LastName  string `json:"last_name" binding:"required"`
-}
